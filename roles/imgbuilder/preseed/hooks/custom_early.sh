4b9b81bb1b8779dca60fe849a58f6ebe6379d7eb
Felix Erkinger <felix@erkinger.at>
2014-11-13 17:38:40 +0100

    typo

--- roles/imgbuilder/preseed/hooks/custom_early.sh
+++ roles/imgbuilder/preseed/hooks/custom_early.sh
@@ -18,9 +18,9 @@ if test "$cmd" = "copy" -o "$cmd" = "fetch"; then
 
     if test "$cmd" = "fetch"; then
         logger -t custom_early.sh "fetch custom.lst and all files included in it"
-        preseed_fetch /custom/custom.lst /tmp/custom.lst
-        for a in `/tmp/custom.lst`; do
-            preseed_fetch $a /tmp/`basename $a`
+        preseed_fetch /custom/custom.lst /tmp/mycustom.lst
+        for a in `/tmp/mycustom.lst`; do
+            preseed_fetch /custom/$a /tmp/`basename $a`
         done
     else
         cp /custom/* /tmp/
