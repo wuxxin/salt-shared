36672c42d160972055f3df2fa6502260caf23c5a
Felix Erkinger <felix@erkinger.at>
2014-11-21 18:08:03 +0100

    wait for ssh available roles.imgbuilder.preseed

--- roles/imgbuilder/preseed/files/set_diskpassword.sh
+++ roles/imgbuilder/preseed/files/set_diskpassword.sh
@@ -31,13 +31,15 @@ if test ! -f ./known_hosts.networkconsole; then
     ssh_opts="$ssh_opts -o StrictHostKeyChecking=no"
 fi
 
-echo -n  "testing access to target: "
+echo -n  "testing ip access to target: "
 until ping $ssh_target -c 4 -q; do
   echo -n .
 done
 
-echo "ok, waiting 5 more seconds for ssh"
-sleep 5
+echo -n "testing ssh access to target: "
+until ssh -o "UserKnownHostsFile=./known_hosts.networkconsole" $ssh_opts root@$ssh_target exit; do
+  echo -n .
+done
 
 echo "writing new custom.env to ssh_target, "
 echo "you may be asked 1.) for the gpg encryption passphrase and then 2.) for your ssh key phrase"
