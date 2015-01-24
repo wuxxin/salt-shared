674d1e80ef36218f3e69d276183ee85997d284ec
Felix Erkinger <wuxxin@gmail.com>
2015-01-24 22:19:58 +0100

    modified generation of /etc/rc.local to overwrite file instead of appending

--- roles/imgbuilder/packer/templates/linux_script/cleanup.sh
+++ roles/imgbuilder/packer/templates/linux_script/cleanup.sh
@@ -52,11 +52,23 @@ rm -f /etc/ssh/ssh_host_*
 
 if type apt-get >/dev/null 2>&1; then
     echo "insert dpkg-reconfigure openssh-server in /etc/rc.local because ubuntu does not regenerate host keys on startup"
-    cat >> /etc/rc.local << EOF
+    cat > /etc/rc.local << EOF
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+# By default this script does nothing.
 if test ! -f /etc/ssh/ssh_host_rsa_key; then
     echo "regenerate ssh host keys ..."
     DEBIAN_FRONTEND=noninteractive dpkg-reconfigure openssh-server
 fi
+exit 0
 EOF
 fi
 
