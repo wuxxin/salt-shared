f593c230ac140a652b34cea949acf0a597648b48
Felix Erkinger <wuxxin@gmail.com>
2015-04-27 23:31:46 +0200

    begun vivid template and mate for vivid; added qcow parameter to image-file-to-lvm.sh, reminder git url for metadata api server in python

--- roles/imgbuilder/vagrant/files/image-file-to-lvm.sh
+++ roles/imgbuilder/vagrant/files/image-file-to-lvm.sh
@@ -10,16 +10,18 @@ name=`basename $dir`
 
 if test "$2" == ""; then
   cat <<EOF
-usage: $0 id lvmgroup [disksize]
+usage: $0 id lvmgroup [--qcow2] [disksize]
 
-takes every (file type) disk of libvirt machine, 
+takes every file type disk of libvirt machine,
 create a lvm volume of same maximal size,
 copies data to target volume
 unlink old, link new volume to libvirt machine
 
 throttles write to 10MB per second.
+optional parameter qcow2 makes the lvm volume type of "QCOW2" instead of "RAW"
 optional parameter disksize (WIP): manual set a new size
-(can not be smaller than qcow2 max size
+(can not be smaller than qcow2 max size)
+optional parameter 
 
 EOF
   exit 1
@@ -28,17 +30,22 @@ fi
 libvirt_id=$1
 libvirt_name=`virsh dumpxml $libvirt_id --migratable | xmlstarlet sel -t -v "domain/name"`
 lvm_group=$2
+output_type=raw
+if test "$3" == "--qcow2"; then 
+  shift
+  output_type=qcow2
+fi
 disk_size=$3
 our_pid=$$
 
-# create a blkio group, write our pid as affected into it
-throttle_bytes="8192" #"10485760" # 10 MB
-throttle_group="move_image_to_lvm"
-mkdir /sys/fs/cgroup/blkio/$throttle_group
-#echo $our_pid > /sys/fs/cgroup/blkio/$throttle_group/tasks
+# create a blkio group
+throttle_bytes="104857" # 1 MB
+throttle_group="imagemove"
+cgcreate -g blkio:$throttle_group
 
 echo "l_id: $libvirt_id , l_name: $libvirt_name , lvm_group: $lvm_group , our_pid: $our_pid"
 echo "`virsh dumpxml $libvirt_id --migratable`"
+ls /sys/fs/cgroup/blkio/
 read answer
 
 # for each of the configured disks in the libvirt xml file
@@ -46,6 +53,7 @@ for disk in `virsh dumpxml $libvirt_id --migratable | xmlstarlet sel -I -t -m "d
   disk_file="${disk%%:*}"
   disk_dev="${disk##*:}"
   lvm_volume=${libvirt_name}.${disk_dev}
+
   # calculate needed disk size
   disk_size_response=`qemu-img info "$disk_file" | grep virtual.size`
   disk_size=`echo "$disk_size_response" | sed -re "s/virtual.size:.[0-9.]+[KMGT].\(([0-9]+).bytes\)/\\1/g"`
@@ -54,7 +62,6 @@ for disk in `virsh dumpxml $libvirt_id --migratable | xmlstarlet sel -I -t -m "d
   read answer
 
   # create lvm volume
-  # virsh vol-create-as poolname newvol 10G
   lvcreate --name $lvm_volume --size ${disk_size}b $lvm_group
   vmm_resp=$(stat -c '%t:%T' `readlink -e /dev/mapper/${lvm_group}-${lvm_volume} `)
   vol_maj_min=$(printf "%d:%d\n" 0x${vmm_resp%%:*} 0x${vmm_resp##*:})
@@ -62,18 +69,17 @@ for disk in `virsh dumpxml $libvirt_id --migratable | xmlstarlet sel -I -t -m "d
   # start write throtteling to created lvm device
   echo "vmm_resp: $vmm_resp , vol_maj_min: $vol_maj_min , throttle_bytes: $throttle_bytes"
   read answer
-  echo "$vol_maj_min  $throttle_bytes" >> /sys/fs/cgroup/blkio/$throttle_group/blkio.throttle.write_bps_device
-  
+  echo "$vol_maj_min $throttle_bytes" > /sys/fs/cgroup/blkio/$throttle_group/blkio.throttle.write_bps_device
+
   # detach original disk from domain
-  virsh detach-disk --domain $libvirt_id --target $disk_file --config
+  #virsh detach-disk --domain $libvirt_id --target $disk_file --config
 
   # copy/convert disk to lvm volume
-  cgexec -g blkio:$throttle_group qemu-img convert -f qcow2 -O raw $disk_file /dev/mapper/${lvm_group}-${lvm_volume}
+  cgexec -g blkio:$throttle_group qemu-img convert -O $output_type  $disk_file /dev/mapper/${lvm_group}-${lvm_volume}
   err=$?
 
   # remove throtteling
-  #echo $our_pid > /sys/fs/cgroup/blkio/tasks
-  rm -r /sys/fs/cgroup/blkio/$throttle_group
+  cgremove blockio:$throttle_group
 
   if test -z $err; then
 
@@ -87,4 +93,3 @@ for disk in `virsh dumpxml $libvirt_id --migratable | xmlstarlet sel -I -t -m "d
     #virsh vol-remove
   fi
 done
-
---
+++ roles/libvirt/metadata.sls
@@ -0,0 +1,1 @@
+https://github.com/wuxxin/libvirt_metadata_api.git
