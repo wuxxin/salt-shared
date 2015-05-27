39a29d4b3ea2a32d60f20757b8e601cca0154843
Felix Erkinger <wuxxin@gmail.com>
2015-05-28 00:45:40 +0200

    update for googlecompute

--- roles/imgbuilder/packer/templates/linux_script/base.sh
+++ roles/imgbuilder/packer/templates/linux_script/base.sh
@@ -3,7 +3,6 @@ if type apt-get >/dev/null 2>&1; then
     apt-get -y update
     apt-get -y upgrade
     apt-get -y install wget curl openssh-server ca-certificates
-    apt-get clean
 fi
 if type yum >/dev/null 2>&1; then
     yum -y update
