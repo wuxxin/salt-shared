c30e96d827daacbb5746d739b3794c4d855341d4
Felix Erkinger <felix@erkinger.at>
2016-01-08 14:00:36 +0100

    minor cosmetic to mozilla addons downloader

--- roles/desktop/mozilla/get-addons.sh
+++ roles/desktop/mozilla/get-addons.sh
@@ -8,8 +8,10 @@ EOF
 fi
 
 baseurl="https://versioncheck.addons.mozilla.org/update/VersionCheck.php?reqVersion=2"
-thunderbird="&appID={3550f703-e582-4d05-9a08-453d09bdfdc6}&appVersion=24.5.0&appOS=Linux&appABI=x86_64-gcc3&locale=de-AT&currentappVersion=24.5.0"
-firefox="&appID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}&appVersion=29.0&appOS=Linux&appABI=x86_64-gcc3&locale=de-AT&currentappVersion=29.0"
+tbver="24.5.0"
+ffver="29.0"
+thunderbird="&appID={3550f703-e582-4d05-9a08-453d09bdfdc6}&appVersion=$tbver&appOS=Linux&appABI=x86_64-gcc3&locale=de-AT&currentappVersion=$tbver"
+firefox="&appID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}&appVersion=$ffver&appOS=Linux&appABI=x86_64-gcc3&locale=de-AT&currentappVersion=$ffver"
 # &updateType=97&compatMode=normal
 
 if test "$1" == "firefox"; then
@@ -36,4 +38,3 @@ for uuid in `cat $2 | dos2unix | grep -v -- "- "| tr -d " \t"`; do
         echo "not found updatelink in rdf: $rdf"
     fi
 done
-
