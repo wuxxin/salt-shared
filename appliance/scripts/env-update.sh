dae8d09684cb18c0414d9a8a1e4cf1749be7206f
Felix Erkinger <felix@erkinger.at>
2017-12-08 18:05:38 +0100

    added appliance/scripts/ env-create.sh/sls, env-update.sh, added executable filemode to *.sh

---
+++ appliance/scripts/env-update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+realpath=$(dirname $(readlink -e "$0"))
+if test -e $realpath/env.functions.sh; then
+    # we are called from the repository and not from a installed appliance, correct paths
+    . $realpath/env.functions.sh
+else
+    . /usr/local/share/appliance/env.functions.sh
+fi
+
+userdata_yaml=$(get_userdata)
+if test $? -ne 0; then
+    printf "Error reading userdata: %s\n" $(echo "$userdata_yaml"| grep USERDATA_ERR)
+    exit 1
+fi
+
+printf "found user-data: %s\n" "$(printf "%s" "$userdata_yaml" | grep USERDATA_TYPE)"
+printf "write userdata to /run/active-env.yml\n"
+printf "%s" "$userdata_yaml" > /run/active-env.yml
+chmod 0600 /run/active-env.yml
--- appliance/scripts/prepare-appliance.sh
+++ appliance/scripts/prepare-appliance.sh
@@ -8,5 +8,12 @@ if test "$APPLIANCE_DOMAIN" != "$(hostname -f)"; then
     hostnamectl set-hostname $APPLIANCE_DOMAIN
 fi
 
+if test "$APPLIANCE_FLAGS_LEN" != ""; then
+    for i in $(seq 0 $(( $APPLIANCE_FLAGS_LEN -1 )) ); do
+        fieldname="APPLIANCE_FLAGS_${i}"; fname="${!fieldname}"
+        touch /app/etc/flags/$fname
+    done
+fi
+        
 appliance_status "Appliance Startup" "Starting up"
 run_hook appliance-prepare startup
