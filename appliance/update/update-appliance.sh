4012e558496fbd97697df4f438b2e3fc698378cf
Felix Erkinger <felix@erkinger.at>
2017-12-19 13:50:44 +0100

    add metric include in check/update-appliance.sh

--- appliance/update/update-appliance.sh
+++ appliance/update/update-appliance.sh
@@ -2,6 +2,7 @@
 
 . /usr/local/share/appliance/env.functions.sh
 . /usr/local/share/appliance/appliance.functions.sh
+. /usr/local/share/appliance/metric.functions.sh
 . /usr/local/share/appliance/update.functions.sh
 
 userdata_to_env appliance || exit $?
