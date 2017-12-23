2d62446c7561ebdb53ccd5f06f2c5d77951a5e7e
Felix Erkinger <felix@erkinger.at>
2017-12-23 23:08:35 +0100

    change hostname on demand while appliance-prepare

--- appliance/scripts/appliance-prepare.sh
+++ appliance/scripts/appliance-prepare.sh
@@ -6,6 +6,14 @@ if test "$APPLIANCE_DOMAIN" != "$(hostname -f)"; then
     # set hostname from env if different
     echo "setting hostname to $APPLIANCE_DOMAIN"
     hostnamectl set-hostname $APPLIANCE_DOMAIN
+    shortid=$(hostname -f | sed -r "s/^([^.]+)\..*/\1/")
+    intip="127\.0\.1\.1"
+    if ! grep -E -q "^${intip}[[:space:]]+${APPLIANCE_DOMAIN}[[:space:]]+${shortid}" /etc/hosts; then
+        grep -q "^${intip}" /etc/hosts && \
+        sed --in-place=.bak -r "s/^(${intip}[ \t]+).*/\1${APPLIANCE_DOMAIN} ${shortid}/" /etc/hosts || \
+        sed --in-place=.bak -r "$ a${intip}\t${APPLIANCE_DOMAIN} ${shortid}" /etc/hosts
+    fi
+    echo -n "${APPLIANCE_DOMAIN}" > /etc/salt/minion_id
 fi
 
 if test "$APPLIANCE_FLAGS_ENABLED_LEN" != ""; then
