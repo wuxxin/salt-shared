634f885e9d3f810ea5ed9f8e9c4ed7dc3add4f8e
Felix Erkinger <felix@erkinger.at>
2018-09-09 01:34:41 +0200

    modify rancherserver env

--- lab/rancher/rancher-agent-setup.sh
+++ lab/rancher/rancher-agent-setup.sh
@@ -8,13 +8,13 @@ set -e
 while ! curl -k https://$RANCHERSERVER/ping; do sleep 3; done
 
 # Generate agent image
-AGENTIMAGE=$(curl -s -H "Authorization: Bearer $APITOKEN" "https://$RANCHERSERVER/v3/settings/agent-image" --insecure | jq -r .value)
+AGENTIMAGE=$(curl -s "$RANCHERSERVER/v3/settings/agent-image" -H "Authorization: Bearer $APITOKEN" --insecure | jq -r .value)
 
 # Generate token (clusterRegistrationToken)
-AGENTTOKEN=$(curl -s "https://$RANCHERSERVER/v3/clusterregistrationtoken" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"type":"clusterRegistrationToken","clusterId":"'$CLUSTERID'"}' --insecure | jq -r .token)
+AGENTTOKEN=$(curl -s "$RANCHERSERVER/v3/clusterregistrationtoken" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"type":"clusterRegistrationToken","clusterId":"'$CLUSTERID'"}' --insecure | jq -r .token)
 
 # Retrieve CA certificate and generate checksum
-CACHECKSUM=$(curl -s -H "Authorization: Bearer $APITOKEN" "https://$RANCHERSERVER/v3/settings/cacerts" --insecure | jq -r .value | sha256sum | awk '{ print $1 }')
+CACHECKSUM=$(curl -s "$RANCHERSERVER/v3/settings/cacerts" -H "Authorization: Bearer $APITOKEN" --insecure | jq -r .value | sha256sum | awk '{ print $1 }')
 
 # write agent environment
 mkdir -p /etc/rancher
--- lab/rancher/rancher-server-setup.sh
+++ lab/rancher/rancher-server-setup.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 set -e 
 
-RANCHERSERVER="{{ settings.server.name }}:{{ settings.server.https_port }}"
+RANCHERSERVER="https://{{ settings.server.name }}:{{ settings.server.https_port }}"
 
 # wait for server
-while ! curl -k https://$RANCHERSERVER/ping; do sleep 3; done
+while ! curl -k $RANCHERSERVER/ping; do sleep 3; done
 
 # try to login with password from environment
-LOGINRESPONSE=$(curl -s "https://$RANCHERSERVER/v3-public/localProviders/local?action=login" -H 'content-type: application/json' --data-binary '{"username":"admin","password":"{{ settings.server.password }}"}' --insecure || true)
+LOGINRESPONSE=$(curl -s "$RANCHERSERVER/v3-public/localProviders/local?action=login" -H 'content-type: application/json' --data-binary '{"username":"admin","password":"{{ settings.server.password }}"}' --insecure || true)
 LOGINTOKEN=`echo $LOGINRESPONSE | jq -r .token || true`
 
 if test "$LOGINTOKEN" = ""; then
     # try with default credentials, change credentials
-    LOGINRESPONSE=$(curl -s "https://$RANCHERSERVER/v3-public/localProviders/local?action=login" -H 'content-type: application/json' --data-binary '{"username":"admin","password":"admin"}' --insecure)
+    LOGINRESPONSE=$(curl -s "$RANCHERSERVER/v3-public/localProviders/local?action=login" -H 'content-type: application/json' --data-binary '{"username":"admin","password":"admin"}' --insecure)
     LOGINTOKEN=`echo $LOGINRESPONSE | jq -r .token`
-    
-    # Change password
-    curl -s "https://$RANCHERSERVER/v3/users?action=changepassword" -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary '{"currentPassword":"admin","newPassword":"{{ settings.server.password }}"}' --insecure
 fi
 
+# Change password
+curl -s "$RANCHERSERVER/v3/users?action=changepassword" -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary '{"currentPassword":"admin","newPassword":"{{ settings.server.password }}"}' --insecure
+
 # Create API key
-APIRESPONSE=$(curl -s "https://$RANCHERSERVER/v3/token" -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary '{"type":"token","description":"automation"}' --insecure)
+APIRESPONSE=$(curl -s "$RANCHERSERVER/v3/token" -H 'content-type: application/json' -H "Authorization: Bearer $LOGINTOKEN" --data-binary '{"type":"token","description":"automation"}' --insecure)
 # Extract and store token
 APITOKEN=$(echo $APIRESPONSE | jq -r .token)
 
+# Set server-url
+curl -s "$RANCHERSERVER/v3/settings/server-url" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" -X PUT --data-binary '{"name":"server-url","value":"'$RANCHERSERVER'"}' --insecure > /dev/null
+
 # Get-XXX or Create cluster
-CLUSTERRESPONSE=$(curl -s "https://$RANCHERSERVER/v3/cluster" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"type":"cluster","nodes":[],"rancherKubernetesEngineConfig":{"ignoreDockerVersion":true},"name":"{{ settings.server.cluster }}"}' --insecure)
+CLUSTERRESPONSE=$(curl -s "$RANCHERSERVER/v3/cluster" -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" --data-binary '{"type":"cluster","nodes":[],"rancherKubernetesEngineConfig":{"ignoreDockerVersion":true},"name":"{{ settings.server.cluster }}"}' --insecure)
 # Extract clusterid
 CLUSTERID=$(echo $CLUSTERRESPONSE | jq -r .id)
 
