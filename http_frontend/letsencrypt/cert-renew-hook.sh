664d491d36e4771612bf8e09f4290dca88a88a80
Felix Erkinger <felix@erkinger.at>
2020-05-21 02:08:46 +0200

    fix renewhook to work as user

--- http_frontend/letsencrypt/cert-renew-hook.sh
+++ http_frontend/letsencrypt/cert-renew-hook.sh
@@ -12,13 +12,10 @@ fi
 simple_metric ssl_cert_acme_renew counter \
     "timestamp since last renew of letsencrypt cert" "$(date +%s)000"
 
-install -o "{{ settings.user }}" -g "{{ settings.group }}" -m "0640" -T \
-        "$KEYFILE" "{{ settings.cert_dir }}/server.key.pem"
-install -o "{{ settings.user }}" -g "{{ settings.group }}" -m "0640" -T \
-        "$FULLCHAINFILE" "{{ settings.cert_dir }}/{{ settings.ssl_chain_cert }}"
+install -m "0640" -T "$KEYFILE" "{{ settings.cert_dir }}/server.key.pem"
+install -m "0640" -T "$FULLCHAINFILE" "{{ settings.cert_dir }}/{{ settings.ssl_chain_cert }}"
 cat "{{ settings.cert_dir }}/{{ settings.ssl_chain_cert }}" "{{ settings.cert_dir}}/{{ settings.ssl_dhparam }}" > "{{ settings.cert_dir}}/{{ settings.ssl_full_cert }}"
 chmod 0640 "{{ settings.cert_dir }}/{{ settings.ssl_full_cert }}"
-chown "{{ settings.user }}:{{ settings.group }}" "{{ settings.cert_dir }}/{{ settings.ssl_full_cert }}"
 
 valid_until=$(openssl x509 -in "$CERTFILE" -enddate -noout | sed -r "s/notAfter=(.*)/\1/g")
 simple_metric ssl_cert_acme_valid_until gauge \
