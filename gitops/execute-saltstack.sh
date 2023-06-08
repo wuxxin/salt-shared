ce557a31f11b603bad9e15b57ad61bbc2acb397b
Felix Erkinger <wuxxin@gmail.com>
2023-06-08 02:44:39 +0200

    reformat, add fix for salt on manjaro

--- gitops/execute-saltstack.sh
+++ gitops/execute-saltstack.sh
@@ -3,9 +3,8 @@ set -eo pipefail
 # set -x
 self_path=$(dirname "$(readlink -e "$0")")
 
-
 usage() {
-    cat << EOF
+    cat <<EOF
 Usage:  $0  [--from-ppa]
             [--etc-dir <minion-config-dir>]
             [--config <list-of-paths> --states <list-of-paths>]
@@ -25,13 +24,15 @@ EOF
     exit 1
 }
 
-
 minion_config() { # $1=base_path $2=config_path $3=config_list $4=states_list
     # generating $config_path/minion config file
     local base_path config_path config_list states_list
-    base_path=$1; config_path=$2; config_list=$3; states_list=$4
+    base_path=$1
+    config_path=$2
+    config_list=$3
+    states_list=$4
     mkdir -p "$config_path"
-    cat << EOF > "$config_path/minion"
+    cat <<EOF >"$config_path/minion"
 id: $(hostname)
 log_level_logfile: info
 file_client: local
@@ -53,18 +54,17 @@ grains:
 EOF
 }
 
-
 salt_install() { # $1=salt_from_ppa(true|false)
     local os_release os_codename os_distributor os_architecture
     local i salt_from_ppa salt_major_version salt_repo_aptline salt_repo_keyfile
     salt_from_ppa="$1"
     os_release=$(lsb_release -r -s)
     os_codename=$(lsb_release -c -s)
-    os_distributor=$(lsb_release  -i -s | tr '[:upper:]' '[:lower:]')
+    os_distributor=$(lsb_release -i -s | tr '[:upper:]' '[:lower:]')
     os_hardware=$(uname -m)
     if test "$os_hardware" = "x86_64"; then
         os_architecture="amd64"
-    elif [[ "$os_hardware" =~ ^(i386|i486|i586|i686)$ ]] ; then
+    elif [[ "$os_hardware" =~ ^(i386|i486|i586|i686)$ ]]; then
         os_architecture="i386"
     else
         os_architecture=$os_hardware
@@ -78,43 +78,54 @@ salt_install() { # $1=salt_from_ppa(true|false)
                 salt_repo_keyfile="https://${salt_ppa_url}/${os_distributor}/${os_release}/${os_architecture}/${salt_major_version}/SALTSTACK-GPG-KEY.pub"
                 echo "installing saltstack ($salt_major_version) from ppa"
                 curl -L -s "$salt_repo_keyfile" | apt-key add -
-                echo "$salt_repo_aptline" > /etc/apt/sources.list.d/saltstack.list
+                echo "$salt_repo_aptline" >/etc/apt/sources.list.d/saltstack.list
                 DEBIAN_FRONTEND=noninteractive apt-get update --yes
             fi
         fi
         DEBIAN_FRONTEND=noninteractive apt-get install -y \
-            salt-minion python3-pytoml curl git gnupg git-crypt
+            curl git gnupg git-crypt salt-minion python3-pytoml
         # keep minion from running automatically
         for i in disable stop mask; do systemctl $i salt-minion; done
     elif test "$os_distributor" = "manjaro"; then
-        pamac install --no-confirm --no-upgrade salt curl git gnupg git-crypt
+        pamac install --no-confirm --no-upgrade curl git gnupg git-crypt salt python-pygit2 python-looseversion
     elif test "$os_distributor" = "arch"; then
-        pacman -Sy salt curl git gnupg git-crypt
+        pacman -Sy curl git gnupg git-crypt salt python-pygit2
     fi
 }
-
 
 main() {
     config_path="/etc/salt"
     config_list="config"
     states_list="salt/salt-shared salt/local"
     from_ppa="false"
-    if test "$1" = "--from-ppa"; then from_ppa="true"; shift; fi
-    if test "$1" = "--etc-dir"; then config_path="$2"; shift 2; fi
-    if test "$1" = "--config"; then config_list="$2"; shift 2; fi
-    if test "$1" = "--states"; then states_list="$2"; shift 2; fi
+    if test "$1" = "--from-ppa"; then
+        from_ppa="true"
+        shift
+    fi
+    if test "$1" = "--etc-dir"; then
+        config_path="$2"
+        shift 2
+    fi
+    if test "$1" = "--config"; then
+        config_list="$2"
+        shift 2
+    fi
+    if test "$1" = "--states"; then
+        states_list="$2"
+        shift 2
+    fi
     if test ! -e "$1"; then usage; fi
     base_path="$(readlink -e $1)"
     shift
     if test "${config_path:0:1}" != "/"; then
         config_path="$base_path/$config_path"
     fi
-    if which cloud-init > /dev/null; then
+    if which cloud-init >/dev/null; then
         printf "waiting for cloud-init finish..."
         cloud-init status --wait || printf "exited with error: $?"
         printf "\n"
     fi
-    if ! which salt-call > /dev/null; then
+    if ! which salt-call >/dev/null; then
         salt_install "$from_ppa"
     fi
     minion_config "$base_path" "$config_path" "$config_list" "$states_list"
